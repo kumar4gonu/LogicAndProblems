97ee5621696d86766a1221b9054fe48f80c2ff77
kumar4gonu <47925670+kumar4gonu@users.noreply.github.com>
2023-01-14 21:01:32 +0530

    Max 3 element in array.

--- com/array/Find3MaxElement.java
+++ com/array/Find3MaxElement.java
@@ -1,4 +1,43 @@
 package com.array;
 
+
+import java.util.Arrays;
+import java.util.Collections;
+
+//Find the largest three distinct elements in an array
+//Input: arr[] = {10, 4, 3, 50, 23, 90}
+//        Output: 90, 50, 23
 public class Find3MaxElement {
+    public static void main(String[] args) {
+      int arr[] = {10, 4, 3, 50, 23, 90};
+       int first, second, thrid;
+       first = second = thrid = Integer.MIN_VALUE;
+      if(arr.length < 3)
+          System.out.println("Please length should be greater then 3.");
+
+      for(int i = 0; i < arr.length;i++){
+          if(arr[i] > first){
+                thrid = second;
+                second = first;
+                first = arr[i];
+
+           }else if(arr[i] > second) {
+              thrid = second;
+              second = arr[i];
+
+          }else if(arr[i] > thrid)
+               thrid = arr[i];
+
+      }
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(thrid);
+
+
+        //Using java 8
+        System.out.println("Using java 8.");
+        Arrays.stream(arr).boxed().sorted(Collections.reverseOrder()).limit(3).forEach(System.out::println);
+
+    }
 }
